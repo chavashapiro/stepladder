8b4dbf70af62779eba966ba4b3ab53d3363ce801
Naami Englard <Naamibe@gmail.com>
2016-05-25 22:19:30 -0400

    fix xml listener

--- src/main/java/com/stepLadder/OfyHelper.java
+++ src/main/java/com/stepLadder/OfyHelper.java
@@ -1,7 +1,6 @@
 package com.stepLadder;
 
-import com.googlecode.objectify.Objectify;
-import com.googlecode.objectify.ObjectifyFactory;
+
 import com.googlecode.objectify.ObjectifyService;
 
 import javax.servlet.ServletContextListener;
--- src/main/java/com/stepLadder/SignGuestbookServlet.java
+++ src/main/java/com/stepLadder/SignGuestbookServlet.java
@@ -2,17 +2,9 @@
 //[START all]
 package com.stepLadder;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
+
 
 import java.io.IOException;
-import java.util.Date;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
